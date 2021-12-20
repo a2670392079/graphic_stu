1e4ec65e38d6041b00a08f9753b7f2c1f123705a
a2670392079 <2670392079@qq.com>
2021-12-20 18:46:09 +0800

    feat(svg): build svg

--- src/svg/index.js
+++ src/svg/index.js
@@ -4,5 +4,18 @@ function getUnlockSvg(width = '200', heigth = '200' ) {
     icon.setAttribute('width', width);
     icon.setAttribute('height', heigth);
     icon.setAttribute('viewBox', "0 0 1024 1024");
-    
+    const lineRect = createTag('rect', {
+        x: 160,
+        y: 96,
+        rx: 864,
+        
+    })
+}
+
+function createTag(tag,objAttr){
+    var oTag = document.createElementNS(svgNS,tag);
+    for(var attr in objAttr){
+        oTag.setAttribute(attr,objAttr[attr]);
+    }
+    return oTag;
 }
